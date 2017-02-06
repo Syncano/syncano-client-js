32d31cc6155d550f3dfa996dd1149c133272710c
mkucharz <m@kucharz.net>
2017-02-06 19:11:11 +0100

    fix(ci): tweaking up scripts

--- tests/unit/index.js
+++ tests/unit/index.js
@@ -1,7 +1,7 @@
 const chai = require('chai')
 const chaiAsPromised = require('chai-as-promised')
 const fetchMock = require('fetch-mock')
-const SyncanoClient = require('../src').default
+const SyncanoClient = require('../../src').default
 
 chai.use(chaiAsPromised)
 
